202bf0a664e1d6895180265d3c6487f312890a9d
dubaokun <dubaokun@gmail.com>
2018-09-10 09:17:40 +0800

    thread_pool

---
+++ ml_code/onehot/onehot.py
@@ -0,0 +1,12 @@
+from sklearn import preprocessing
+
+encoder = preprocessing.OneHotEncoder()
+encoder.fit([
+    [0, 2, 1, 12],
+    [1, 3, 5, 3],
+    [2, 3, 2, 12],
+    [1, 2, 4, 3]
+])
+encoded_vector = encoder.transform([[2, 3, 5, 3]]).toarray()
+print("\n Encoded vector =", encoded_vector)
+
