504f536fa3daadff7e680a6a5243a3e5f1afc8c9
dubaokun <dubaokun@gmail.com>
2018-09-27 18:02:07 +0800

    ssh py

---
+++ ssh_to.py
@@ -0,0 +1,20 @@
+﻿
+# $language = "python"
+# $interface = "1.0"
+
+# This automatically generated script may need to be
+# edited in order to work correctly.
+
+# @author WangZongQi
+
+import os
+import sys
+
+def Main():
+    host = crt.Arguments[0] 
+    password = crt.Arguments[1]
+    crt.Screen.Synchronous = True
+    crt.Screen.Send("ssh " + host + chr(13))
+    if True == crt.Screen.WaitForString("password:", 5):
+        crt.Screen.Send(password + chr(13))
+Main()
