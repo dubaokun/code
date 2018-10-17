60fb94954a014998a3f246458547da4550fd4a37
dubaokun <dubaokun@gmail.com>
2018-10-17 18:51:24 +0800

    compile

--- code_style/test.cpp
+++ code_style/test.cpp
@@ -3,12 +3,35 @@
  */
 
 #include <iostream>
+#include <string>
 
-int main() 
-{
+#ifndef IN
+#define IN
+#endif
 
+#ifndef OUT
+#define OUT
+#endif
+
+void foo(const std::string &str) {}
+std::string a = "hello";
+std::string b = "world";
+
+void test(IN const std::string& a, OUT std::string& b) {
+    //std::string b = a;
+    int aa = 1;
+    const long &bb = aa;
+    std::string sa = "aaa";
+    const std::string& sb = sa;
+}
+
+void test2() {
+    using std::string;
+    string a = "sas";
+
+int main() {
     std::cout << "hello !" << std::endl;
+    foo(a + b);
     return 0;
-
 }
 
