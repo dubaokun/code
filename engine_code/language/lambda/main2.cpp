9978826f19882c131467e0c24fb8c00c391664b1
dubaokun <dubaokun@gmail.com>
2018-11-02 10:36:39 +0800

    lambda

---
+++ engine_code/language/lambda/main2.cpp
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+/*  
+ *  Author : 
+ *      Sam, 2018-11-02 
+ *
+ *
+ *  Brief:
+ *      1.parameters; 2.return value; 3.function body  
+ *
+ */
+
+
+=======
+>>>>>>> 565d4624f486f6bb385ccb612602abb433ddf090
+#include <string>
+#include <iostream>
+
+// lambda expression with no parameters
+auto basicLambda = [] { std::cout << "lambda test" << std::endl;};
+// lambda expression with parameters
+auto basicLambda1 = [](std::string str) { std::cout << "lambda test=" << str << std::endl; };
+// function point
+void fileFuck() {
+    std::cout << "function point!" << std::endl;
+}
+
+void Test1() {
+    // lambda expression with no parameters 
+    basicLambda();
+    // lambda expression with parameters
+    basicLambda1("basicLambda1");
+    // function point
+    void (*funcp) ();       // function point declaration
+    funcp = fileFuck;       // function point define
+    (*funcp)();             // funttion call
+}
+
+<<<<<<< HEAD
+void Test2() {
+    int x = 10;
+
+    auto add_x = [x](int a) { return a + x; }; 
+    auto multiply_x = [&x](int a) { return a * x; }; 
+
+    std::cout << add_x(10) << " " << multiply_x(10) << std::endl;
+}
+
+int main(int argc, char** argv) {
+    Test1();
+    Test2();
+=======
+int main(int argc, char** argv) {
+    Test1();
+>>>>>>> 565d4624f486f6bb385ccb612602abb433ddf090
+    return 0;
+}
