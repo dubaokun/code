e4fc01a4b8f24102387ec085d2dfab299c6464fb
dubaokun <dubaokun@gmail.com>
2018-11-07 09:10:25 +0800

    heapsort

--- engine_code/algorithm/heapsort/heapsort.cpp
+++ engine_code/algorithm/heapsort/heapsort.cpp
@@ -1,68 +1,76 @@
-/*堆排序(大顶堆) 2011.9.14*/ 
-
+/***************************************************************************
+ *  @file       main.cpp
+ *  @author     MISAYAONE
+ *  @date       25  March 2017
+ *  @remark     25  March 2017 
+ *  @theme      Heap Sort 
+ ***************************************************************************/
+ 
 #include <iostream>
-#include<algorithm>
+#include <vector>
+#include <time.h>
+#include <Windows.h>
 using namespace std;
-
-void HeapAdjust(int *a,int i,int size)  //调整堆 
+ 
+//辅助交换函数
+void Swap(int &a, int &b)
 {
-    int lchild=2*i;       //i的左孩子节点序号 
-    int rchild=2*i+1;     //i的右孩子节点序号 
-    int max=i;            //临时变量 
-    if(i<=size/2)          //如果i是叶节点就不用进行调整 
-    {
-        if(lchild<=size&&a[lchild]>a[max])
-        {
-            max=lchild;
-        }    
-        if(rchild<=size&&a[rchild]>a[max])
-        {
-            max=rchild;
-        }
-        if(max!=i)
-        {
-            swap(a[i],a[max]);
-            HeapAdjust(a,max,size);    //避免调整之后以max为父节点的子树不是堆 
-        }
-    }        
+	int temp = a;
+	a = b;
+	b = temp;
 }
-
-void BuildHeap(int *a,int size)    //建立堆 
+ 
+//堆排序的核心是建堆,传入参数为数组，根节点位置，数组长度
+void Heap_build(int a[],int root,int length)
 {
-    int i;
-    for(i=size/2;i>=1;i--)    //非叶节点最大序号值为size/2 
-    {
-        HeapAdjust(a,i,size);    
-    }    
-} 
-
-void HeapSort(int *a,int size)    //堆排序 
+	int lchild = root*2+1;//根节点的左子结点下标
+	if (lchild < length)//左子结点下标不能超出数组的长度
+	{
+		int flag = lchild;//flag保存左右节点中最大值的下标
+		int rchild = lchild+1;//根节点的右子结点下标
+		if (rchild < length)//右子结点下标不能超出数组的长度(如果有的话)
+		{
+			if (a[rchild] > a[flag])//找出左右子结点中的最大值
+			{
+				flag = rchild;
+			}
+		}
+		if (a[root] < a[flag])
+		{
+			//交换父结点和比父结点大的最大子节点
+			Swap(a[root],a[flag]);
+			//从此次最大子节点的那个位置开始递归建堆
+			Heap_build(a,flag,length);
+		}
+	}
+}
+ 
+void Heap_sort(int a[],int len)
 {
-    int i;
-    BuildHeap(a,size);
-    for(i=size;i>=1;i--)
-    {
-        //cout<<a[1]<<" ";
-        swap(a[1],a[i]);           //交换堆顶和最后一个元素，即每次将剩余元素中的最大者放到最后面 
-          //BuildHeap(a,i-1);        //将余下元素重新建立为大顶堆 
-          HeapAdjust(a,1,i-1);      //重新调整堆顶节点成为大顶堆
-    }
-} 
-
-int main(int argc, char *argv[])
+	for (int i = len/2; i >= 0; --i)//从最后一个非叶子节点的父结点开始建堆
+	{
+		Heap_build(a,i,len);
+	}
+ 
+	for (int j = len-1; j > 0; --j)//j表示数组此时的长度，因为len长度已经建过了，从len-1开始
+	{
+		Swap(a[0],a[j]);//交换首尾元素,将最大值交换到数组的最后位置保存
+		Heap_build(a,0,j);//去除最后位置的元素重新建堆，此处j表示数组的长度，最后一个位置下标变为len-2
+	}
+ 
+}
+int main(int argc, char **argv)
 {
-     //int a[]={0,16,20,3,11,17,8};
-    int a[100];
-    int size;
-    while(scanf("%d",&size)==1&&size>0)
-    {
-        int i;
-        for(i=1;i<=size;i++)
-            cin>>a[i];
-        HeapSort(a,size);
-        for(i=1;i<=size;i++)
-            cout<<a[i]<<"";
-        cout<<endl;
-    }
-    return 0;
+	clock_t Start_time = clock();
+	int a[10] = {12,45,748,12,56,3,89,4,48,2};
+	Heap_sort(a,10);
+ 	for (size_t i = 0; i != 10; ++i)
+ 	{
+ 		cout<<a[i]<<" ";
+ 	}
+	clock_t End_time = clock();
+	cout<<endl;
+	cout<<"Total running time is: "<<static_cast<double>(End_time-Start_time)/CLOCKS_PER_SEC*1000<<" ms"<<endl;
+	cin.get();
+	return 0;
 }
