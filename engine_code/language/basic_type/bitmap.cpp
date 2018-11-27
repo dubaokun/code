7095e6eaaa378005d09f962536f4819a4f67384c
dubaokun <dubaokun@gmail.com>
2018-11-27 19:22:45 +0800

    bitmap

---
+++ engine_code/language/basic_type/bitmap.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+const static int ALL_SET = ~0;
+
+int main(int argc, char ** argv) {
+    int a  = -2;
+    cout << ALL_SET << endl;
+    return 0;
+}
