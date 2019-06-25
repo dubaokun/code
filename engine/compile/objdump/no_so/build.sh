8fd914ce91c2efb5d9ca557aa398f919dbf62511
dubaokun <dubaokun@jd.com>
2019-06-25 14:45:19 +0800

    整理文件夹

--- engine/compile/objdump/no_so/build.sh
+++
@@ -1,3 +0,0 @@
-g++ -g -fPIC -shared -o libmax.so max.cpp
-#g++ -g test.cpp -L. -lmax -lboost_thread -Wl,-rpath=./
-g++ -g test.cpp 
--- engine/compile/objdump/so/build.sh
+++
@@ -1,2 +0,0 @@
-g++ -g -fPIC -shared -o libmax.so max.cpp
-g++ -g test.cpp -L. -lmax -Wl,-rpath=./
