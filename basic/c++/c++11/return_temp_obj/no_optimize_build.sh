0d5cc86c328c3848067dfba2dda2fb9e8802e826
dubaokun <dubaokun@jd.com>
2019-06-30 02:24:51 +0800

    修改编译命令

--- basic/c++/c++11/return_temp_obj/no_optimize_build.sh
+++ basic/c++/c++11/return_temp_obj/no_optimize_build.sh
@@ -1,3 +1,3 @@
-g++ constructor.cpp -o constructor -fno-elide-constructors
-g++ --std=c++11 constructor11.cpp -o constructor11 -fno-elide-constructors
-g++ --std=c++11 constructor12.cpp -o constructor12 -fno-elide-constructors
+g++ constructor.cpp -o constructor.out -fno-elide-constructors
+g++ --std=c++11 constructor11.cpp -o constructor11.out -fno-elide-constructors
+g++ --std=c++11 constructor12.cpp -o constructor12.out -fno-elide-constructors
