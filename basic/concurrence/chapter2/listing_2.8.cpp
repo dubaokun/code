8dacadb604e0ec43bcb02dff6acd20ffcf2cfeaf
dubaokun <dubaokun@jd.com>
2019-06-30 02:21:16 +0800

    concurrence

--- basic/concurrence/chapter2/listing_2.8.cpp
+++ basic/concurrence/chapter2/listing_2.8.cpp
@@ -19,40 +19,40 @@ T parallel_accumulate(Iterator first,Iterator last,T init)
 {
     unsigned long const length=std::distance(first,last);
 
-    if(!length)
+    if(!length)  // 1
         return init;
 
     unsigned long const min_per_thread=25;
     unsigned long const max_threads=
-        (length+min_per_thread-1)/min_per_thread;
+        (length+min_per_thread-1)/min_per_thread;  // 2
 
     unsigned long const hardware_threads=
         std::thread::hardware_concurrency();
 
-    unsigned long const num_threads=
+    unsigned long const num_threads=  // 3
         std::min(hardware_threads!=0?hardware_threads:2,max_threads);
 
-    unsigned long const block_size=length/num_threads;
+    unsigned long const block_size=length/num_threads; // 4
 
-    std::vector<T> results(num_threads);
+    std::vector<T> results(num_threads);  // 5
     std::vector<std::thread>  threads(num_threads-1);
 
     Iterator block_start=first;
     for(unsigned long i=0;i<(num_threads-1);++i)
     {
         Iterator block_end=block_start;
-        std::advance(block_end,block_size);
-        threads[i]=std::thread(
+        std::advance(block_end,block_size);  // 6
+        threads[i]=std::thread(  // 7
             accumulate_block<Iterator,T>(),
             block_start,block_end,std::ref(results[i]));
-        block_start=block_end;
+        block_start=block_end;  // 8
     }
-    accumulate_block<Iterator,T>()(block_start,last,results[num_threads-1]);
+    accumulate_block<Iterator,T>()(block_start,last,results[num_threads-1]);  // 9
     
     std::for_each(threads.begin(),threads.end(),
-        std::mem_fn(&std::thread::join));
+        std::mem_fn(&std::thread::join));  // 10
 
-    return std::accumulate(results.begin(),results.end(),init);
+    return std::accumulate(results.begin(),results.end(),init); // 11
 }
 
 int main()
