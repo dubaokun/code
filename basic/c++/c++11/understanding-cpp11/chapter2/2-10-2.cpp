171f61d5ec58349627a4f55a6f5f608411e1d758
dubaokun <dubaokun@jd.com>
2019-07-09 07:03:53 +0800

    重载 重写 隐藏

--- basic/c++/c++11/understanding-cpp11/chapter2/2-10-2.cpp
+++ basic/c++/c++11/understanding-cpp11/chapter2/2-10-2.cpp
@@ -1,11 +1,20 @@
+#include <iostream>
+
+
 struct Object{
     virtual void fun() = 0;
 };
 
 struct Base : public Object {
-    void fun() final;   // 声明为final
+    void fun() {}   // 声明为final
 };
 
 struct Derived : public Base {
-    void fun();     // 无法通过编译
+    void fun(int a) {std::cout << "a" << std::endl;}     // 无法通过编译
 };
+
+int main(int argc, char** argv) {
+	Derived derive;
+	derive.fun(1);
+	return 0;
+}
