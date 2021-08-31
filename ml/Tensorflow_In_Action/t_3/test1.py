3b5d201c48fd1518d513a19540be2ff6ee45ba03
dubaokun <dubaokun@gmail.com>
2021-09-01 01:40:56 +0800

    nn

---
+++ ml/Tensorflow_In_Action/t_3/test1.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+ 
+g1 = tf.Graph()
+with g1.as_default():
+	v=tf.get_variable("v",shape=[2, 3],initializer=tf.zeros_initializer())
+
+g2 = tf.Graph()
+with g2.as_default():
+	v=tf.get_variable("v",shape=[1],initializer=tf.ones_initializer())
+
+with tf.Session(graph=g1) as sess:
+	tf.initialize_all_variables().run()
+	with tf.variable_scope("",reuse=True):
+		print(sess.run(tf.get_variable("v")))
+
+with tf.Session(graph=g2) as sess:
+	tf.initialize_all_variables().run()
+	with tf.variable_scope("",reuse=True):
+		print(sess.run(tf.get_variable("v")))
