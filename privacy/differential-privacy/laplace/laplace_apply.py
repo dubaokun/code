6ff893a6b55314df19b870aa1355a083ae74e6aa
dubaokun <dubaokun@ZBMAC-C02D5257M.local>
2021-11-18 16:50:42 +0800

    differential privacy

--- privacy/differential-privacy/laplace/laplace_apply.py
+++ privacy/differential-privacy/laplace/laplace_apply.py
@@ -1,7 +1,7 @@
 import numpy as np
  
  # 计算基于拉普拉斯分布的噪声
-def laplace_noisy_count(sensitivety,epsilon):
+def laplace_noisy(sensitivety,epsilon):
     beta = sensitivety/epsilon
     u1 = np.random.random()
     u2 = np.random.random()
@@ -10,13 +10,13 @@ def laplace_noisy_count(sensitivety,epsilon):
         n_value = -beta*np.log(1.-u2)
     else:
         n_value = beta*np.log(u2)
-    print(n_value)
+    print("Add Noisy = %.16f " % n_value)
     return n_value
 
 # 计算基于拉普拉斯加噪的混淆值
 def laplace_mech(data, sensitivety, epsilon):
     for i in range(len(data)):
-        data[i] += laplace_noisy_count(sensitivety,epsilon)
+        data[i] += laplace_noisy(sensitivety,epsilon)
     return data
  
 # 基于拉普拉斯分布的特性，如果想要分布震荡较小，需要将隐私预算epsilon的值设置较大
@@ -26,4 +26,4 @@ if __name__ =='__main__':
     epsilon = 10
     data_noisy = laplace_mech(data, sensitivety, epsilon)
     for j in data_noisy:
-        print(j)
+        print("Final Resulet = %.16f" % j)
