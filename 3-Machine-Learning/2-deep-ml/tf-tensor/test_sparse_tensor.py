b984dca7f2260531d0ac603ca8f60ca437390a4b
dubaokun <dubaokun@gmail.com>
2022-06-02 06:04:40 +0800

    loss

---
+++ 3-Machine-Learning/2-deep-ml/tf-tensor/test_sparse_tensor.py
@@ -0,0 +1,9 @@
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
+sp = tf.SparseTensor(indices=[[0,2],[1,3]], values=[1,2], dense_shape=[3,4])
+with tf.Session() as tf:
+        print('*********************');
+        print(sp.eval())
+        print('*********************')
