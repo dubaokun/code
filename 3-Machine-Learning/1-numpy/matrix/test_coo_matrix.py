2444e3fc650f3badf7f7e1d6777baa176e5126df
dubaokun <dubaokun@gmail.com>
2024-05-29 11:06:08 +0800

    matrix

--- 3-Machine-Learning/1-numpy/matrix/test_coo_matrix.py
+++ 3-Machine-Learning/1-numpy/matrix/test_coo_matrix.py
@@ -1,7 +1,17 @@
+# 坐标格式的矩阵(Coodrdinate format matrix)
+"""
+优点：
+  1.不同稀疏格式间转换效率高(exp:CSR/CSC)
+  2.coo_matrix不支持元素的存取和增删，一旦创建之后，除了将之转换成其它格式的矩阵，几乎无法对其做任何操作和矩阵运算。
+  3.构建矩阵时，允许坐标重复
+缺点：
+  1.不能直接运算
+  2.不能直接切片操作
+"""
 import scipy.sparse as sp
-data = [1, 1, 2]
+data = [1, 1, 1]
 row = [0, 1, 1]
-col = [0, 1, 2]
+col = [0, 1, 1]
 matrix = sp.coo_matrix((data, (row, col)), shape=(3, 3))
 #matrix.todense()
 print("================")
--- 3-Machine-Learning/1-numpy/matrix/test_csr_matrix.py
+++ 3-Machine-Learning/1-numpy/matrix/test_csr_matrix.py
@@ -1,3 +1,19 @@
+"""
+csr_matrix: 压缩稀疏行矩阵(Compressed sparse row matrix)
+优点：
+
+高效的矩阵加法与乘法内积运算
+高效的行切片操作
+CSR格式在存储稀疏矩阵时非零元素平均使用的字节数(Bytes per Nonzero
+Entry)最为稳定（float类型约为8.5，double类型约为12.5）CSR格式常用于读入数据后进行稀疏矩阵计算。
+缺点：
+
+列切片操作慢（相比CSC)
+转换成稀疏结构成本高（相比LIL)
+
+https://www.cnblogs.com/jzYe/p/17806891.html
+
+"""
 import scipy.sparse as sp
 import numpy as np
 indptr = np.array([0, 2, 3, 6])
